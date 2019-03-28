0bcc4c1fb1c683a49e0101cec82cb05d0ea5d536
Administrator <administrator@CN-2.lan>
2019-03-28 12:53:48 +0000

    Updates

---
+++ precache-manifest.4f14f5cfac1ac8f032065dff104dad5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d483c7686c9eb27d17fb",
+    "url": "/To-do-list/static/js/runtime~main.61cf1b59.js"
+  },
+  {
+    "revision": "b307b249538d1b629f97",
+    "url": "/To-do-list/static/js/main.a6a9f41c.chunk.js"
+  },
+  {
+    "revision": "b548e30793887743f0b8",
+    "url": "/To-do-list/static/js/2.cf331bc2.chunk.js"
+  },
+  {
+    "revision": "b307b249538d1b629f97",
+    "url": "/To-do-list/static/css/main.34e87eb4.chunk.css"
+  },
+  {
+    "revision": "ba6197fe24fb5e1855d12a63ac92b164",
+    "url": "/To-do-list/index.html"
+  }
+];
